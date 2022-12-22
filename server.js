1a4dbf636e34f075895ae33b974907c5430e9b55
SimrithaReddy <95024988+SimrithaReddy@users.noreply.github.com>
2022-12-22 22:46:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const Getproperty = require('./routes/get')
 const jwt = require('jsonwebtoken')
 dotenv.config();
 const app = express();
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+app.use(cors(corsOptions));
 
 app.use("/properties", (req, res, next) => {
   let token = req.headers.authorization;
@@ -23,7 +29,6 @@ app.use("/properties", (req, res, next) => {
   }
 })
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
