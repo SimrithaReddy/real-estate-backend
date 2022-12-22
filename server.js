d2393935e52e1f901a4ad4ba6a43a590e890c565
SimrithaReddy <simrithareddyk04@gmail.com>
2022-12-22 10:56:45 +0530

    backend done real-estate

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require('dotenv')
+const cors = require('cors')
+const addProperty = require('./routes/addproperty');
+const loginPage = require('./routes/login');
+const registerPage = require('./routes/register')
+const Getproperty = require('./routes/get')
+const jwt = require('jsonwebtoken')
+dotenv.config();
+const app = express();
+
+app.use("/properties", (req, res, next) => {
+  let token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+      if (err)  return res.status(403).send("User not authorized")
+      req.user_mail = decoded
+      next();
+  })
+  }else{
+    res.send("User not authorized")
+  }
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+app.use("/properties/posts", addProperty);
+app.use('/login', loginPage);
+app.use('/register', registerPage)
+app.use('/properties', Getproperty)
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to DB");
+  }
+);
+
+app.listen(process.env.PORT, () => {
+  console.log("server is up at port 5000")
+})  
