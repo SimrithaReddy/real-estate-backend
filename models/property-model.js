10d66cfdfee60f2b4f11c3e4be8cec609b177f77
SimrithaReddy <95024988+SimrithaReddy@users.noreply.github.com>
2022-12-23 23:54:05 +0530

    Update property-model.js

--- models/property-model.js
+++ models/property-model.js
@@ -2,107 +2,107 @@ const mongoose = require("mongoose")
 const propertyschema = new mongoose.Schema({
     propertyType: {
         type: String,
-        required:true
+        require: [true, "Please Select Property Type"]
     },
     price: {
         type: String,
-        required:true
+        require: [true, "Please Add Price"]
     },
     propertyAge: {
         type: String,
-        required:true
+        require: [true, "Please Select Property Age"]
     },
     propertyDiscription: {
         type: String,
-        required:true
+        require: [true, "Please Add Property Discription"]
     },
     negotiable: {
         type: String,
-        required:true
+        require: [true, "Please Select Negotiable Type"]
     },
     ownership: {
         type: String,
-        required:true
+        require: [true, "Please Select Ownership Type"]
     },
     propertyApproved: {
         type: String,
-        required:true
+        require: [true, "Please Select Property Approved Type"]
     },
     bankLoan: {
         type: String,
-        required:true
+        require: [true, "Please Select Bank Loan Type"]
     },
     length: {
         type: String,
-        required:true
+        require: [true, "Please Add Length"]
     },
     breadth: {
         type: String,
-        required:true
+        require: [true, "Please Add Breadth"]
     },
     totalArea: {
         type: String,
-        required:true
+        require: [true, "Please Add Total Area"]
     },
     areaUnit: {
         type: String,
-        required:true
+        require: [true, "Please Add Area Unit"]
     },
     noOfBHK: {
         type: String,
-        required:true
+        require: [true, "Please Select Number of BHK"]
     },
     noOfFloor: {
         type: String,
-        required:true
+        require: [true, "Please Select Number of Floor"]
     },
     attached: {
         type: String,
-        required:true
+        require: [true, "Please Select Attached Type"]
     },
     westernToilet: {
         type: String,
-        required:true
+        require: [true, "Please Select Western Toilet Type"]
     },
     furnished: {
         type: String,
-        required:true
+        require: [true, "Please Select Furnished Type"]
     },
     carParking: {
         type: String,
-        required:true
+        require: [true, "Please Select Car Parking Type"]
     },
     lift: {
         type: String,
-        required:true
+        require: [true, "Please Select Lift Type"]
     },
     electricity: {
         type: String,
-       required:true
+        require: [true, "Please Add Electricity Phase"]
     },
     facing: {
         type: String,
-        required:true
+        require: [true, "Please Select Facing Type"]
     },
     name: {
         type: String,
-        required:true
+        require: [true, "Please Select Owner Type"]
     },
     mobile: {
         type: String,
-        required:true
+        require: [true, "Please Add Mobile Number"]
     },
     postedBy: {
         type: String,
-        required:true
+        require: [true, "Please Select posted By Type"]
     },
     saleType: {
         type: String,
-        required:true
+        require: [true, "Please Select Sale Type"]
     },
     featuredPackage: {
         type: String,
-        required:true
+        require: [true, "Please Select Featured Package Type"]
     },
     PPDPackage: {
         type: String,
@@ -110,11 +110,11 @@ const propertyschema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required:true
+        require: [true, "Please Add Email"]
     },
     city: {
         type: String,
-        required:true
+        require: [true, "Please Select City"]
     },
     area: {
         type: String,
@@ -122,11 +122,11 @@ const propertyschema = new mongoose.Schema({
     },
     pincode: {
         type: String,
-        required:true
+        require: [true, "Please Select Pincode"]
     },
     address: {
         type: String,
-        required:true
+        require: [true, "Please Add Address"]
     },
     landmark: {
         type: String,
